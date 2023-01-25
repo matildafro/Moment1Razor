304f1fa12532060c6a2c2b4dca38a84ba596cedf
Matilda Fröberg <matilda.froberg@live.se>
2023-01-25 15:31:20 +0100

    Add project files.

---
+++ Moment1Razor/Pages/Razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment1Razor.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Moment1Razor/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// Hitta knapp via ID:
+let mybutton = document.getElementById("myBtn");
+
+// När användaren scrollar 20px ner visas knappen
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// När knappen används - åk upp till toppen
+function topFunction() {
+    document.body.scrollTop = 0; // För Safari
+    document.documentElement.scrollTop = 0; // För Chrome, Firefox, IE and Opera
+}
